3190eeaa767b96dd2cc5a1675811e3b283133b8d
Apeksha Tembhurne <appy17@users.noreply.github.com>
2025-01-12 19:19:04 +0530

    new

--- js/script.js
+++ js/script.js
@@ -26,39 +26,50 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2024 00:00:00").getTime();
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 09, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get todays date and time
     var now = new Date().getTime();
     
-    // Find the distance between now and the count down date
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
+    // Time calculations for days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    // Output the result in an element with id="time"
+    document.getElementById("time").innerHTML = `
+        <div class='container'>
+            <div class='days block'>${days}<br>Days</div>
+            <div class='hours block'>${hours}<br>Hours</div>
+            <div class='minutes block'>${minutes}<br>Minutes</div>
+            <div class='seconds block'>${seconds}<br>Seconds</div>
+        </div>`;
     
-    // If the count down is over, write some text 
+    // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("time").innerHTML = "Bless the married couple for a happy life!";
     }
 }, 1000);
+
+// Cool console logs for added effect
+console.log('%cSAVE THE DATE: 9th Feb, 2025!', 'background: linear-gradient(#D33106, #571402); border: 4px solid #3E0E02; color: white; text-align: center; font-weight: bold; font-size: 32px; line-height: 40px;');
+console.log('%cYour presence is requested!', 'color: #FF6C37; text-shadow: 0 2px 0 rgba(0, 0, 0, 1); font-size: 32px; font-weight: bold;');
+console.log('%cShaadi me zaroor aana!', 'color: yellow; background: tomato; font-size: 24pt; font-weight: bold;');
+
 
 // being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
     , 'color: white'
-    , 'display: block'
+    , 'display: flex'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
     , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
     , 'line-height: 40px'
@@ -69,7 +80,7 @@ var styles = [
 
 var styles1 = [
     'color: #FF6C37'
-    , 'display: block'
+    , 'display: flex'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
     , 'line-height: 40px'
     , 'font-weight: bold'
@@ -78,18 +89,18 @@ var styles1 = [
 
 var styles2 = [
     'color: teal'
-    , 'display: block'
+    , 'display: flex'
     , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
     , 'line-height: 40px'
     , 'font-weight: bold'
     , 'font-size: 32px'
 ].join(';');
 
-// console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
 
-// console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
 
-// console.log(
-//     `%cShaadi me zaroor aana!\n\n`,
-//     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-// )
+console.log(
+    `%cShaadi me zaroor aana!\n\n`,
+    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
+)
